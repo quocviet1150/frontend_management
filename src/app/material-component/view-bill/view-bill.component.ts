0358199aedfa79ad633293abe6b20927fd14e19c
vietnq <khoahocmaytinh37@gmail.com>
2024-01-12 00:37:16 +0700

    vietnq

--- src/app/material-component/view-bill/view-bill.component.ts
+++ src/app/material-component/view-bill/view-bill.component.ts
@@ -7,6 +7,8 @@ import { BillService } from 'src/app/services/bill.service';
 import { SnackbarService } from 'src/app/services/snackbar.service';
 import { GlobalConstants } from 'src/app/shared/global-constants';
 import { ViewBillProductsComponent } from '../dialog/view-bill-products/view-bill-products.component';
+import { ConfirmationComponent } from '../dialog/confirmation/confirmation.component';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-view-bill',
@@ -63,11 +65,52 @@ export class ViewBillComponent implements OnInit {
     })
   }
 
-  handleDeleteAction(value: any){
+  handleDeleteAction(value: any) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {
+      message: ' xóa hóa đơn' + value.name,
+      confirmation: true
+    }
+    const dialogRef = this.dialog.open(ConfirmationComponent);
+    const sub = dialogRef.componentInstance.onEmitstatusChange.subscribe((response) => {
+      this.deleteBill(value.id);
+      dialogRef.close();
+    })
 
   }
 
-  handleDowloadAction(value:any){
-    
+  deleteBill(id: any) {
+    this.billService.delete(id).subscribe((response: any) => {
+      this.tableData();
+      this.responseMessage = response?.message;
+      this.snackbarService.openSnackbar(this.responseMessage, "success")
+    }, (error: any) => {
+      console.log(error.error?.message);
+      if (error.error?.message) {
+        this.responseMessage = error.error?.message;
+      } else {
+        this.responseMessage = GlobalConstants.genericError;
+      }
+      this.snackbarService.openSnackbar(this.responseMessage, GlobalConstants.error)
+    })
+  }
+
+  handleDowloadAction(value: any) {
+    var data = {
+      name: value.name,
+      email: value.email,
+      uuid: value.uuid,
+      contactNumber: value.contactNumber,
+      paymentMethod: value.paymentMethod,
+      totalAmount: value.total.toString(),
+      productDetail: value.productDetail,
+    }
+    this.downloadFile(value.uuid, data)
+  }
+
+  downloadFile(fileName: string, data: any) {
+    this.billService.getPdf(data).subscribe((response) => {
+      saveAs(response, fileName + '.pdf');
+    })
   }
 }
